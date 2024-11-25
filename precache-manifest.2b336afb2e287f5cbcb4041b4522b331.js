dc8f5e6d48d4227fa9218ecad6a21d4847338f7f
ItsEND <waweda299@gmail.com>
2024-11-25 19:41:29 +0300

    Add files via upload

---
+++ precache-manifest.2b336afb2e287f5cbcb4041b4522b331.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fd1414c32caf4b5d383ef539c14f1e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "576adf1d1c0b8ab63e8c",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "e266eec883dfbc0ffb9a",
+    "url": "/static/js/2.1a240082.chunk.js"
+  },
+  {
+    "revision": "576adf1d1c0b8ab63e8c",
+    "url": "/static/js/main.33d37f22.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
